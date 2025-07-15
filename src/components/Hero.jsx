fd551558809f0bd7935b729fbf635e7884bedd1e
Harshad Nikam <100086117+dev-harshhh19@users.noreply.github.com>
2025-07-16 00:19:54 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -123,7 +123,7 @@ const Hero = () => {
             <motion.a
               whileHover={{ scale: 1.05 }}
               whileTap={{ scale: 0.95 }}
-              file: `${import.meta.env.BASE_URL}public\Resume\HarshadNikamResume.pdf"
+              href={`${import.meta.env.BASE_URL}Resume/HarshadNikamResume.pdf`}
               download
               target="_blank"
               rel="noopener noreferrer"
