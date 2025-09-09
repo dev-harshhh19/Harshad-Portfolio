991456010b2efa55c64a6f50f1e267ff8df48685
Harshad Nikam <100086117+dev-harshhh19@users.noreply.github.com>
2025-09-09 13:59:38 +0530

    Update Profile.jsx

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,7 +4,7 @@ const Profile = () => {
   return (
     <div className="w-full h-full flex items-center justify-center">
       <img
-        src="https://res.cloudinary.com/dtsque0dg/image/upload/v1757406035/profile-min_rzgdcv.png"
+        src="https://res.cloudinary.com/dtsque0dg/image/upload/v1757406533/1000169302_u6spoe.jpg"
         alt="Profile"
         className="rounded-full w-50 h-50 object-cover"
       />
