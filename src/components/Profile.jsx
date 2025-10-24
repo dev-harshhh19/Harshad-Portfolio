ea01fb9dce6bd76f3a58c85a84576eb3f8127f11
NIKAM HARSHAD SHIVAJI <100086117+dev-harshhh19@users.noreply.github.com>
2025-10-24 12:40:08 +0530

    Initial Commit

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,7 +4,7 @@ const Profile = () => {
   return (
     <div className="w-full h-full flex items-center justify-center">
       <img
-        src="https://res.cloudinary.com/dtsque0dg/image/upload/v1759176939/1000174031_cunuik.jpg"
+        src="https://res.cloudinary.com/dtsque0dg/image/upload/v1761289671/Profile_image_y1n580.jpg"
         alt="Profile"
         className="rounded-full w-50 h-50 object-cover"
       />
