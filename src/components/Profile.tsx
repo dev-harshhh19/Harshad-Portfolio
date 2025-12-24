ce0ea32a5cb9e93cab39ff524fff65cf354bc4c7
dev-harshhh19 <harshadnikam7516@gmail.com>
2025-12-24 14:43:07 +0530

    feat: replace img tag with Next.js Image component in Profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import Image from "next/image";
 
 const Profile = () => {
   return (
     <div className="w-full h-full flex items-center justify-center">
-      <img
-        src="https://res.cloudinary.com/dtsque0dg/image/upload/v1761289671/Profile_image_y1n580.jpg"
+      <Image
+        src="https://res.cloudinary.com/dtsque0dg/image/upload/f_auto,q_auto,w_200,c_fill/v1761289671/Profile_image_y1n580.jpg"
         alt="Profile"
-        className="rounded-full w-50 h-50 object-cover"
+        width={200}
+        height={200}
+        priority
+        className="rounded-full object-cover"
+        sizes="200px"
       />
     </div>
   );
